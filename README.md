439e37b0a718ed1a7dc41375214cfdec7fc6a6ad
Galaxy-Wanderer <raduonu@yahoo.com>
2020-03-21 16:04:24 +0200

    Added contact page

---
+++ README.md
@@ -0,0 +1,2 @@
+# Random Website
+A random website with my best layout design skills(It's not John Doe's website)
--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-/*const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.style.transition = '1s ease';
-            entry.target.style.color = 'blue';
-        }
-    });
-}, {
-    threshold: 0.7
-});
-observer.observe(document.querySelector('h1'));*/
+// burger navigation
 const burger = document.querySelector('.burger');
 const links = document.querySelector('.links');
 burger.addEventListener('click', () => {
     links.classList.toggle('links-active');
     burger.classList.toggle('burger-active');
 });
+
+// counter
+const counters = document.querySelectorAll('.counter');
+const speed = 2000;
+const updateCount = () => {
+    counters.forEach(counter => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increase = target / speed;
+        if(count < target) {
+            counter.innerText = count + increase;
+            setTimeout(updateCount, 1);
+        } else {
+            count.innerText = target;
+        }
+    });
+}
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            updateCount();
+        }
+    });
+}, {
+    threshold: 0.3
+});
+observer.observe(document.querySelector('.stats'));
+
+// form preventer
+document.querySelector('form').addEventListener('submit', e => e.preventDefault());
